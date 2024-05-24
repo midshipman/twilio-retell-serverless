ef8010c61ebddab63a1ed714a7a0bef64a1c2560
hwang <readercn@gmail.com>
2024-05-25 00:04:37 +0200

    added update bot prompt when airtable form submited

--- src/functions/update-bot.ts
+++ src/functions/update-bot.ts
@@ -1,10 +1,42 @@
+import Airtable from 'airtable';
+import Retell from "retell-sdk";
 
+exports.handler =  async function (context, event, callback) {
 
-export const handler = async function(context, event, callback) {
-    console.log(event.id);
-   
-
+    console.log('Incoming', event.id);
     
+    try {
+        
+        var base = new Airtable({apiKey: context.AIRTABLE_API_KEY,}).base(context.AIRTABLE_BASE_ID);
 
-    return callback(null);
-}
+        // const events = await base('builder').select({
+        //     fields: ["Name", "Bookings"],
+        //     maxRecords: 1,
+        //   }).all();
+        
+        // console.log('Retrieved', events[0]);
+
+        var record =  base('builder').find(event.id);
+        if (record) {
+            
+            var description = "Description: \n" + (await record).get('Description') + "\n";
+            var flow = "Voice Flow: \n" + (await record).get('Voice Flow') + "\n";
+            var plan = "Flights Plan: \n " + (await record).get('Flights Plan');
+            var bookings = "Bookings: \n" + (await record).get('Bookings') + "\n";
+
+            var prompt = description +  plan + bookings + flow;
+            console.log(`new prompt: \n`, prompt);
+
+            // update the prompt to retell agent
+            const retell = new Retell({apiKey: context.RETELL_API_KEY,});
+            await retell.llm.update(context.RETELL_LLM_ID, {general_prompt:prompt});
+            // retell.llm.update(context.RETELL_LLM_ID, {model:"gpt-4o"});
+        }
+    }
+    catch (err) {
+        console.error("Error connecting to Airtable", err);
+        return callback(null, "Sorry, something went wrong, please try again!");
+    }
+
+    return callback(null, "Your Voice Assistant is ready!");
+};
